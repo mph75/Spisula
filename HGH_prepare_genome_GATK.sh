ebd7fa5bdb2c8f5cb823a7b5ce71c3b0134e23c3
hannahhartung <haha4191@colorado.edu>
2020-03-24 11:24:31 -0600

    Organize and upload new script

---
+++ HGH_prepare_genome_GATK.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# index reference genome for bwa, create fasta indexes (fai and dict)
+
+TMP=/workdir/$USER/tmp
+GATKDIR=/programs/gatk-4.1.4.0
+export PATH=$GATKDIR:$PATH
+
+# cd genome
+
+# Genome summary files needed and by GATK tools
+gatk CreateSequenceDictionary -R Solidis_ref.fa  -O Solidis_ref.fa.dict
+samtools faidx Solidis_ref.fa
+
+# index for BWA alignment
+bwa index Solidis_ref.fa
+
+# index image file needed by some Spark-based tools (if used)
+gatk --java-options "-Djava.io.tmpdir=$TMP" BwaMemIndexImageCreator \
+     -I Solidis_ref.fa \
+     -O Solidis_ref.fa.img
